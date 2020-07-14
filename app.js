a6ea45c9d6695f47502b62c2c8953a6825d331d2
SaravanaaVelpari <thisissaravanaa@gmail.com>
2020-07-14 14:25:14 +0530

    vs checking

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Oye Oye Suvetha, my pappu!');
+  res.send('Oye Oye Suvetha, i am a genius!');
 });
 app.listen(process.env.PORT, function () {
   console.log('Example app listening on heroku port!');
